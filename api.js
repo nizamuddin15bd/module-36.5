5af94b8092958f43dc3d0d8a57086d93c92be646
nizamuddin15bd <nizamuddin15bd@gmail.com>
2022-11-12 19:59:42 +0600

    api.js

---
+++ api.js
@@ -0,0 +1,37 @@
+const loadSingleUser = () => {
+    fetch('https://randomuser.me/api/')
+        .then(res => res.json())
+        .then(data => displaySingleUser(data.results[0]))
+}
+loadSingleUser();
+
+
+const displaySingleUser = user => {
+    console.log(user);
+}
+
+
+const loadMeals = searchText => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMeals(data.meals))
+}
+
+const displayMeals = meals => {
+    const container = document.getElementById('meals')
+    meals.forEach(meal => {
+        console.log(meal);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <h1>${meal.strMeal}</h1>
+        <button onclick ="loadMealsDetails('${meal.strMeal}')">click me</button>
+        `;
+        container.appendChild(div);
+    })
+}
+loadMeals('fish');
+
+const loadMealsDetails = mealName => {
+    console.log(mealName);
+}
